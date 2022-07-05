d251ea2b2638d49d8bb34a796f13b36f458fd989
Marcos Aparicio <marcos.aparicio593@gmail.com>
2022-07-05 12:52:00 -0500

    focus to bad input when there was a mistake on the input & now just bad inputs are deleted ON EDIT.PHP

--- edit.php
+++ edit.php
@@ -29,13 +29,27 @@
   }
 
   $error = null;
+  $errorType = null;
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
     if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
+
       $error = "Please fill all the fields.";
+      $contact["name"] = $_POST["name"];
+      $contact["phone_number"] = $_POST["phone_number"];
+
+      $errorType = empty($_POST["name"])? 1:2;
+    
     } else if (strlen($_POST["phone_number"]) < 9) {
+
       $error = "Phone number must be at least 9 characters.";
+      $contact["name"] = $_POST["name"];
+      $contact["phone_number"] = "";
+      $errorType = 2;
+      
     } else {
+
       $name = $_POST["name"];
       $phoneNumber = $_POST["phone_number"];
 
@@ -57,6 +71,7 @@
   }
 ?>
 
+
 <?php require "partials/header.php" ?>
 
 <div class="container pt-5">
@@ -70,20 +85,21 @@
               <?= $error ?>
             </p>
           <?php endif ?>
-          <form method="POST" action="edit.php?id=<?= $contact['id'] ?>">
+          <form method="POST" action="edit.php?id=<?= $contact['id'] ?>" name="input">
             <div class="mb-3 row">
               <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
               <div class="col-md-6">
-                <input value="<?= $contact['name'] ?>" id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
+                <input value="<?= $contact['name'] ?>" id="name" type="text" class="form-control" name="name" autocomplete="name"
+                <?php if($errorType == 1 || $errorType != 2):?> autofocus <?php endif?>
+                >
               </div>
             </div>
 
             <div class="mb-3 row">
               <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
-
               <div class="col-md-6">
-                <input value="<?= $contact['phone_number'] ?>" id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
+                <input value="<?= $contact['phone_number'] ?>" id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" <?php if($errorType == 2):?> autofocus <?php endif?> >
               </div>
             </div>
 
