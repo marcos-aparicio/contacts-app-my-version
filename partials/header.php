0c051626f213b0d07819d506b256d6383f31782b
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-03-07 12:44:47 -0500

    fixing responsiveness

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
-<?php 
-$styles = json_decode(file_get_contents("./theme_changes/colors.json"), true);
-$color = $styles["color"];
+<?php
+$styles = json_decode(file_get_contents('./theme_changes/colors.json'), true);
+$color = $styles['color'];
 
 ?>
 <!DOCTYPE html>
@@ -15,34 +15,30 @@ $color = $styles["color"];
 
 
   <!-- Bootstrap -->
-  <?php if($color != "dark"): ?>
+  <?php if ($color != 'dark'): ?>
     <link
     rel="stylesheet"
     href="static/css/flatly.css"
     />
-  <?php else:?>
+  <?php else: ?>
     <link
     rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css"
-    integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w=="
-    crossorigin="anonymous"
-    referrerpolicy="no-referrer"
+    href="static/css/dark-mode.css"
     />
-  <?php endif?>
-
+  <?php endif ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous" defer></script>
 
   <!-- Static Content -->
   <link rel="stylesheet" href="./static/css/index.css" />
   <script defer src="./static/js/welcome.js"></script>
 
-  
+
   <title>Contacts App</title>
 </head>
 <body>
-  <?php require "navbar.php" ?>
+  <?php require 'navbar.php' ?>
 
-  
-  <?php if (isset($_SESSION["flash"])): ?>
+  <?php if (isset($_SESSION['flash'])): ?>
     <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
       <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
@@ -50,14 +46,14 @@ $color = $styles["color"];
     </svg>
 
     <div class="container mt-4">
-      <div class="alert alert-<?= $_SESSION["flash"]["color"] ?> d-flex align-items-center" role="alert">
+      <div class="alert alert-<?= $_SESSION['flash']['color'] ?> d-flex align-items-center" role="alert">
         <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
         <div class="ml-2">
-          <?= $_SESSION["flash"]["message"] ?>
+          <?= $_SESSION['flash']['message'] ?>
         </div>
       </div>
     </div>
-    <?php unset($_SESSION["flash"]) ?>
+    <?php unset($_SESSION['flash']) ?>
   <?php endif ?>
 
 
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,5 +1,4 @@
-
-<nav class="navbar navbar-expand-lg navbar-<?=$color?> bg-<?=$color?> px-0">
+<nav class="navbar navbar-expand-lg navbar-<?= $color ?> bg-<?= $color ?> px-0">
   <div class="container-fluid">
     <a class="navbar-brand font-weight-bold" href="index.php">
       <img class="mr-2" src="./static/img/logo.png" />
@@ -19,7 +18,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <div class="d-flex justify-content-between w-100 align-items-center">
         <ul class="navbar-nav">
-          <?php if (isset($_SESSION["user"])): ?>
+          <?php if (isset($_SESSION['user'])): ?>
             <li class="nav-item">
               <a class="nav-link" href="home.php">Home</a>
             </li>
@@ -39,19 +38,19 @@
           <?php endif ?>
         </ul>
         <div class="p-2 d-flex align-items-center">
-        <?php if($color == "light"):?>
+        <?php if ($color == 'light'): ?>
           <a class = "border-dark btn rounded-circle" href = "./theme_changes/theme.php?color=dark">
             <i class="fa-solid fa-moon"></i>
           </a>
-        <?php else:?>
+        <?php else: ?>
           <a class = "border-light btn rounded-circle" href = "./theme_changes/theme.php?color=light">
             <i class="fa-solid fa-sun"></i>
           </a>
-        <?php endif?>
+        <?php endif ?>
 
-        <?php if (isset($_SESSION["user"])): ?>
+        <?php if (isset($_SESSION['user'])): ?>
             <span class = "ms-5 ml-5">
-              <?= $_SESSION["user"]["email"] ?>
+              <?= $_SESSION['user']['email'] ?>
             </span>
             <?php endif ?>
         </div>
