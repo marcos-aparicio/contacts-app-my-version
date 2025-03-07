84ba33850b5cb73bc53118267df99444b0b98be7
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-03-07 12:45:21 -0500

    sanitizing colors to only allow the correct ones

--- theme_changes/theme.php
+++ theme_changes/theme.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
 
-//TO DO: poner sanitizacion de variable get color
-$url = $_SERVER["HTTP_REFERER"];
-$styles = json_decode(file_get_contents("colors.json"), true);
-$styles["color"] = $_GET["color"];
+$url = $_SERVER['HTTP_REFERER'];
+$styles = json_decode(file_get_contents('colors.json'), true);
 
+$styles['color'] = $_GET['color'];
 
-file_put_contents("colors.json",json_encode($styles));
+if (!isset($url) || ($styles['color'] !== 'light' && $styles['color'] !== 'dark'))
+    return;
+
+file_put_contents('colors.json', json_encode($styles));
 
 Header("Location: $url");
